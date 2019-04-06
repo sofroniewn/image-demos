eb9566e4ed8b38dced4ef446d70eecdb76c4804c
sofroniewn <sofroniewn@gmail.com>
2019-04-06 09:05:10 -0700

    improve AOLLSM demo

---
+++ demos/AOLLSM.py
@@ -0,0 +1,28 @@
+"""
+Displays an 100GB zarr file of lattice light sheet data
+"""
+
+import numpy as np
+from napari import ViewerApp
+from napari.util import app_context
+# from dask_image.imread import imread
+import dask.array as da
+
+
+file_name = '/Users/nicholassofroniew/Documents/DATA-imaging/ExM/AOLLSM_m4_560nm-6.zarr'
+
+data = da.from_zarr(file_name)
+print(data.shape)
+
+
+# data = da.stack([imread(f, nframes=-1) for f in image_files], axis=-1)
+# print(data.shape)
+# data = np.random.rand(16000, 16000)
+
+clim_range = [0, 150_000]
+
+with app_context():
+    # create an empty viewer
+    viewer = ViewerApp()
+    layer = viewer.add_image(data, multichannel=False, name='AOLLSM_m4_560nm',
+                             clim_range=clim_range)
--- helpers/make_zarr_files.py
+++ helpers/make_zarr_files.py
@@ -7,19 +7,21 @@ import zarr
 image_path = '/Volumes/GoogleDrive/My Drive/d/'
 image_file = 'ex6-2_CamA_ch1_CAM1_stack*_560nm_*Abs_000x_000y_000z_0000t_decon.tif'
 image_files = glob(image_path + image_file)
-# shape = (len(image_files), 201, 1024, 768)
+n_files = len(image_files)
 
 file_name = '/Users/nicholassofroniew/Documents/DATA-imaging/ExM/AOLLSM_m4_560nm-6.zarr'
-
 ### For one file
 # shape = (768, 1024, 201)
 # z1 = zarr.open(file_name, mode='w', shape=shape, chunks=(None, None, 10), dtype='f4')
 # z1[:] = imread(image_files[0]).transpose(2, 1, 0)
 
 ### For many files
-shape = (768, 1024, 201, 100)
-z1 = zarr.open(file_name, mode='w', shape=shape, chunks=(None, None, 4, 4), dtype='f4')
+shape = (768, 1024, 201, n_files)
+z1 = zarr.open(file_name, mode='a', shape=shape, chunks=(None, None, 4, 4), dtype='f4')
 
-for i, f in enumerate(image_files[:100]):
-    print(i)
-    z1[:, :, :, i] = imread(f).transpose(2, 1, 0)
+for i, f in enumerate(image_files):
+    print(i, n_files)
+    if i < 73:
+        pass
+    else:
+        z1[:, :, :, i] = imread(f).transpose(2, 1, 0)

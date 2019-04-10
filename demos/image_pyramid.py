0ad40817d89c772e35838c95c6d07f2431cb4749
sofroniewn <sofroniewn@gmail.com>
2019-04-10 13:51:17 -0700

    improve 2D pyramid example

--- demos/image_pyramid.py
+++ demos/image_pyramid.py
@@ -2,16 +2,20 @@
 Image pyramid of pathology slide
 """
 
-from openslide import OpenSlide
 import numpy as np
+import dask.array as da
+from dask.cache import Cache
 from napari import ViewerApp
 from napari.util import app_context
 
-slide = OpenSlide('data/camelyon16/normal_001.tif')
+cache = Cache(2e9)  # Leverage two gigabytes of memory
+cache.register()
 
-pyramid = ([np.asarray(slide.read_region((0, 0), i,
-                       slide.level_dimensions[i])).transpose(1, 0, 2)
-                       for i in range(4, 10)])
+file_name = 'data/camelyon16/normal_001.zarr'
+
+pyramid = [da.from_zarr(file_name + '/' + str(i)) for i in range(10)]
+
+print([p.shape[:2] for p in pyramid])
 
 with app_context():
     # create an empty viewer
@@ -19,3 +23,5 @@ with app_context():
 
     # add the pyramid
     layer = viewer.add_pyramid(pyramid, name='slide')
+    #shape = np.array(pyramid[0].shape[:2])
+    #viewer.camera.zoom(.1, center=-shape/2)
---
+++ helpers/make_2D_zarr_pathology.py
@@ -0,0 +1,21 @@
+import numpy as np
+import zarr
+from openslide import OpenSlide
+
+slide = OpenSlide('data/camelyon16/normal_001.tif')
+file_name = 'data/camelyon16/normal_001.zarr'
+
+root = zarr.open_group(file_name, mode='a')
+
+
+for i in range(0, 1):
+    print(i, 10)
+    shape = (slide.level_dimensions[i][0], slide.level_dimensions[i][1], 4)
+    z1 = root.create_dataset(str(i), shape=shape, chunks=(300, 300, None),
+                             dtype='uint8')
+
+    for j in range(slide.level_dimensions[i][0]//1528):
+        print(j, slide.level_dimensions[i][0]/1528)
+        image = np.asarray(slide.read_region((j*1528*(2**i), 0), i,
+                           (1528, slide.level_dimensions[i][1]))).transpose(1, 0, 2)
+        z1[j*1528:(j+1)*1528] = image

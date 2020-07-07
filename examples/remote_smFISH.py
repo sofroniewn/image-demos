94cc55e59c2b2d3909f70fb2728c5161263cfa91
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-07-06 19:56:33 -0700

    more ex

--- examples/remote_smFISH.py
+++ examples/remote_smFISH.py
@@ -1,21 +1,29 @@
 """
-Dynamically load irregularly shapes images of bees from s3
+Dynamically load and process data
 """
 
-import numpy as np
-import dask.array as da
-from dask.cache import Cache
 import napari
+import dask.array as da
+from dask_image.ndfilters import gaussian_filter
+from skimage.restoration import richardson_lucy
+import numpy as np
 
-cache = Cache(2e9)  # Leverage two gigabytes of memory
-cache.register()
 
 file_path = 's3://sofroniewn/image-data/smFISH/raw.zarr'
-smFISH = da.from_zarr(file_path)
-print(smFISH.shape)
+raw = da.from_zarr(file_path)
+blurred = gaussian_filter(raw, (0, 2, 2))
+
+x, y = np.meshgrid(np.linspace(-1,1,6), np.linspace(-1,1,6))
+d = np.sqrt(x*x+y*y)
+sigma, mu = 2.0, 0.0
+psf = np.expand_dims(np.exp(-((d-mu)**2 / (2.0 * sigma**2))), axis=0)
+deconvolved = raw.map_blocks(lambda block: richardson_lucy(block, psf, clip=False))
 
 
 with napari.gui_qt():
+
     # create an empty viewer
-    #napari.view_image(path=file_path, name='smFISH', contrast_limits=(140.0, 1300.0))
-    napari.view_image(smFISH, name='smFISH', contrast_limits=(140.0, 1300.0))
+    viewer = napari.Viewer()
+    viewer.add_image(raw, contrast_limits=(140.0, 1200.0))
+    viewer.add_image(blurred, contrast_limits=(140.0, 1200.0))
+    viewer.add_image(deconvolved, contrast_limits=(140.0, 1200.0))

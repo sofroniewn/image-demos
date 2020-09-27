7aa2cff3c9cf5c0bf589b5729f6be6db860d1796
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-09-26 19:48:17 -0700

    fix rotations

--- test-examples/million_points.py
+++ test-examples/million_points.py
@@ -4,7 +4,7 @@
 import numpy as np
 import napari
 
-points = np.random.randint(100, size=(1000, 2))
+points = np.random.randint(100, size=(50_000, 2))
 
 with napari.gui_qt():
     viewer = napari.view_points(points, face_color='red')

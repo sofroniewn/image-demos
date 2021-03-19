79c2620c6fa7b5f0a6c331b0f562e33697d62706
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-03-18 21:41:21 -0700

    update master

--- test-examples/big_labels.py
+++ test-examples/big_labels.py
@@ -4,5 +4,4 @@ import napari
 
 with napari.gui_qt():
     viewer = napari.Viewer()
-    viewer.add_image(np.random.random((2048, 2048)))
-    viewer.add_labels(np.zeros((2048, 2048)))
+    viewer.add_labels(np.ones((2048, 2048)).astype(int))
